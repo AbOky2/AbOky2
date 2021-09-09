2c0f019db779d427abaec8afc240964da1de7b41
okymi <okymi@MacBook-Pro-de-abachou.local>
2021-09-10 01:42:42 +0200

    commit ecommerce

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import imgHomeShop from './shopimg.jpg'
+
+export default function Home() {
+    return (
+        <div className="global-container">
+            <h1 className="home-title">
+                Bienvenue au <span>Mordor</span>
+            </h1>
+            <p>Mordor est un site ecomerce destiné à la vente des mugs personnalisés.</p>
+            <img src={imgHomeShop} alt="accueil shop" />
+        </div>
+    )
+}
